b5a76135f53746cb21c44588b652aac0bb242c06
Roque <roquewagnertadeu@gmail.com>
2025-01-15 14:50:03 -0300

    primeiro commit configurando o projeto usando arquitetura hexagonal

---
+++ src/main/java/com/buscarcep/arq/hex/api_arq_hex/ApiArqHexApplication.java
@@ -0,0 +1,13 @@
+package com.buscarcep.arq.hex.api_arq_hex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiArqHexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiArqHexApplication.class, args);
+	}
+
+}
